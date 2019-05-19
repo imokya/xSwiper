523354647f3ac97ebef89864835dc0f319c16aef
imokya <876657294@qq.com>
2019-05-19 21:49:48 +0800

    commit

--- plugins/manifest-webpack-plugin.js
+++ plugins/manifest-webpack-plugin.js
@@ -14,23 +14,23 @@ class ManifestWebpackPlugin {
     this.src = path.resolve(__dirname, this.options.source)
     this.des = path.resolve(__dirname, this.options.output)
     const manifest = []
-    this.getFolderInfo(this.src, manifest)
+    this.getFolderInfo(manifest)
     const data = JSON.stringify(manifest)
     console.log(manifest)
     fs.writeFileSync(this.des + path.sep + 'manifest.json', data, { flag: 'w' })
   }
 
-  getFolderInfo(folder, manifest, prefix = '') {
-    const info = fs.readdirSync(folder, {
+  getFolderInfo(manifest, prefix = '') {
+    const newPrefix = prefix ?  prefix + '/' : prefix
+    const folder = this.src + path.sep + prefix
+    const result = fs.readdirSync(folder, {
       withFileTypes: true
     })
-    const newPrefix = prefix ?  prefix + '/' : prefix
-    info.forEach((item) => {
+    result.forEach((item) => {
       if(item.isDirectory()) {
-        const newFolder = path.join(folder, item.name)
         const obj = {}
         obj[item.name] = [], manifest.push(obj)
-        this.getFolderInfo(newFolder, obj[item.name], newPrefix + item.name)
+        this.getFolderInfo(obj[item.name], newPrefix + item.name)
       } else {
         manifest.push(newPrefix + item.name)
       }
